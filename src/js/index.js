56a3cc5155d73297aa5a227112238524cac9995b
IhorMasechko <igor.masechko007@gmail.com>
2023-02-09 12:33:42 +0200

    final result

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,8 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const input = document.querySelector('.search-form-input');
-const btnSearch = document.querySelector('.search-form-button');
+const input = document.querySelector('input[name="searchQuery"]');
+const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
@@ -12,47 +12,73 @@ let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 btnLoadMore.style.display = 'none';
 
 let pageNumber = 1;
+let currentHits = 0;
 
-btnSearch.addEventListener('click', e => {
+searchForm.addEventListener('submit', onSubmitSearchForm);
+
+async function onSubmitSearchForm(e) {
   e.preventDefault();
   cleanGallery();
   const trimmedValue = input.value.trim();
   if (trimmedValue !== '') {
-    fetchImages(trimmedValue, pageNumber).then(foundData => {
-      if (foundData.hits.length === 0) {
+    const response = await fetchImages(trimmedValue, pageNumber);
+    currentHits = response.hits.length;
+
+    try {
+      if (response.hits.length === 0) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
-      } else {
-        renderImageList(foundData.hits);
+      } else if (response.hits.length > 0 && response.hits.length < 40) {
+        renderImageList(response.hits);
         Notiflix.Notify.success(
-          `Hooray! We found ${foundData.totalHits} images.`
+          `Hooray! We found ${response.totalHits} images.`
+        );
+        btnLoadMore.style.display = 'none';
+        gallerySimpleLightbox.refresh();
+      } else {
+        renderImageList(response.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${response.totalHits} images.`
         );
         btnLoadMore.style.display = 'block';
         gallerySimpleLightbox.refresh();
       }
-    });
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+}
 
-btnLoadMore.addEventListener('click', () => {
+btnLoadMore.addEventListener('click', onClickLoadMoreBtn);
+
+async function onClickLoadMoreBtn() {
   pageNumber++;
   const trimmedValue = input.value.trim();
   btnLoadMore.style.display = 'none';
-  fetchImages(trimmedValue, pageNumber).then(foundData => {
-    if (foundData.hits.length === 0) {
+  const response = await fetchImages(trimmedValue, pageNumber);
+
+  try {
+    if (response.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-    } else {
-      renderImageList(foundData.hits);
-      Notiflix.Notify.success(
-        `Hooray! We found ${foundData.totalHits} images.`
+    } else if (response.hits.length < 40) {
+      renderImageList(response.hits);
+      btnLoadMore.style.display = 'none';
+      gallerySimpleLightbox.refresh();
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
       );
+    } else {
+      renderImageList(response.hits);
       btnLoadMore.style.display = 'block';
+      gallerySimpleLightbox.refresh();
     }
-  });
-});
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function renderImageList(images) {
   console.log(images, 'images');
@@ -60,7 +86,7 @@ function renderImageList(images) {
     .map(image => {
       console.log('img', image);
       return `<div class="photo-card">
-       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy/></a>
+       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
         <div class="info">
            <p class="info-item">
     <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
@@ -78,7 +104,7 @@ function renderImageList(images) {
     </div>`;
     })
     .join('');
-  gallery.innerHTML += markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function cleanGallery() {
